f802055ddf55e334d9eca595ba4abf6b1a0a0766
lh <360653229@qq.com>
2026-06-30 17:15:10 +0800

    添加Giscus评论系统

--- components/Comments.tsx
+++ components/Comments.tsx
@@ -14,10 +14,10 @@ export default function Comments() {
 
     const script = document.createElement("script");
     script.src = "https://giscus.app/client.js";
-    script.setAttribute("data-repo", "YOUR_USERNAME/YOUR_REPO");
-    script.setAttribute("data-repo-id", "YOUR_REPO_ID");
+    script.setAttribute("data-repo", "liuhao522/memory-blog");
+    script.setAttribute("data-repo-id", "R_kgDOTJUeCA");
     script.setAttribute("data-category", "Announcements");
-    script.setAttribute("data-category-id", "YOUR_CATEGORY_ID");
+    script.setAttribute("data-category-id", "DIC_kwDOTJUeCM4DAMzW");
     script.setAttribute("data-mapping", "pathname");
     script.setAttribute("data-strict", "0");
     script.setAttribute("data-reactions-enabled", "1");
